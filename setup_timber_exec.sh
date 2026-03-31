b63b3e6abb43df2c50ac41f7572e23cc40c4d112
Matej Roguljic <matej.roguljic@gmail.com>
2026-03-31 18:13:11 +0200

    update the docker path

--- setup_timber_exec.sh
+++ setup_timber_exec.sh
@@ -3,6 +3,6 @@ timber_exec() {
         --bind "$(readlink $HOME)" \
         --bind /etc/grid-security/certificates \
         --bind /cvmfs \
-        /cvmfs/unpacked.cern.ch/registry.hub.docker.com/mrogulji/timber:run3/ \
+        /cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/jhu-tools/timber:run3/ \
         python3 "$@"
 }
