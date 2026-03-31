26f7f314875f4915004152e9e00837a61526e15e
Matej Roguljic <matej.roguljic@gmail.com>
2026-03-31 18:12:29 +0200

    preselection exe

---
+++ condor/preselection_wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Wrapper script to run preselection_job.py inside singularity container
+echo HOME: $HOME
+echo X509_USER_PROXY: $X509_USER_PROXY
+ls -l $X509_USER_PROXY
+voms-proxy-info
+
+echo "Running preselection_job.py with arguments: $@"
+
+singularity exec \
+    --bind "$(readlink $HOME)" \
+    --bind /etc/grid-security/certificates \
+    --bind /cvmfs \
+    /cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/jhu-tools/timber:run3/ \
+    python3 condor/preselection_job.py "$@"
