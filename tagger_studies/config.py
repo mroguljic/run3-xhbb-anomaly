34aef1edca9beb188f1dba922c479e40fb017622
Matej Roguljic <matej.roguljic@gmail.com>
2026-06-29 07:29:02 -0500

    optimize taggers wp

---
+++ tagger_studies/config.py
@@ -0,0 +1,31 @@
+MANIFEST_PATH = "condor/template_manifest_2024.json"
+PROCESS_MAPS = {"MX1800_MY100": ["MX1800_MY100"], "QCD": ["QCD-4Jets_HT-600to800","QCD-4Jets_HT-800to1000","QCD-4Jets_HT-1000to1200","QCD-4Jets_HT-1200to1500", "QCD-4Jets_HT-1500to2000", "QCD-4Jets_HT-2000"], "TT": ["TTto4Q", "TTtoLNu2Q"]}
+TEMPLATE_STORE_DIR = "tagger_studies/templates"
+YEAR = "2024"
+SIGNAL = "MX1800_MY100"
+
+
+M_JJ_BINS = (66, 700, 4000)
+M_JY_BINS = (112, 40, 600)
+
+
+SCAN_CONFIG = {
+    "xbb_fixed": 0.95,
+    "antiqcd_fixed": 0.95,
+}
+
+import numpy as np
+antiqcd_vals = np.arange(0.0, 1.0, 0.045)
+xbb_vals = np.arange(0.0, 0.89, 0.045)
+xbb_vals = np.append(xbb_vals, np.arange(0.9, 1.0, 0.01)) #increase density near optimum value
+
+SCAN_CONFIG["xbb_scan_points"] = xbb_vals
+SCAN_CONFIG["antiqcd_scan_points"] = antiqcd_vals
+
+# Hand picked bins for MX1800_MY100 bsed on the following bins
+#M_JJ_BINS = (66, 700, 4000)
+#M_JY_BINS = (112, 40, 600)
+INTEGRAL_X_MIN_BIN = 19
+INTEGRAL_X_MAX_BIN = 24
+INTEGRAL_Y_MIN_BIN = 10
+INTEGRAL_Y_MAX_BIN = 19
--- tagger_studies/optimize_taggerrs.py
+++
@@ -1,3 +0,0 @@
-from condor.template_job_batch import stage_input_files, create_filelist_txt
-
-print("test")
